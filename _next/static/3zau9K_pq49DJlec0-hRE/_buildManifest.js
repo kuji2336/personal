853d7b479efd7efa8df8fd5cc0e2c06bf2657d13
kuji2336 <58769030+kuji2336@users.noreply.github.com>
2022-06-25 19:09:14 +0000

    Deploying to gh-pages from @ kuji2336/personal@0d491802c6029cc49a9f021703ce0718aa0f395f 🚀

---
+++ _next/static/3zau9K_pq49DJlec0-hRE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ce6c0271b8fa626f3557.js"],"/_error":["static/chunks/pages/_error-67fba61e1425a3723b83.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

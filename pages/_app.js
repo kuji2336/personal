6ff333b5905caf09860ac9990630ced2a9c0a6aa
kujx <quji.bolqvadze123@gmail.com>
2022-06-26 00:09:16 +0400

    added analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,32 @@ import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
+import Script from 'next/script'
 
+{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-ELFRGZHGM4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ELFRGZHGM4');
+</script> */}
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Script strategy='lazyOnLoad' src='https://www.googletagmanager.com/gtag/js?id=G-ELFRGZHGM4' />
+			<Script strategy='lazyOnLoad'>
+				{
+					` window.dataLayer = window.dataLayer || [];
+			  function gtag(){dataLayer.push(arguments);}
+			  gtag('js', new Date());
+			
+			  gtag('config', 'G-ELFRGZHGM4')`
+				}
+			</Script>
+			<Component {...pageProps} />;
+		</>
+	)
 }
 
 export default MyApp;
